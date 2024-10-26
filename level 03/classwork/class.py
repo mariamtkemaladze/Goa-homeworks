a67aa27c9fb6c77d355527e7cc369d3fc920b84f
mariamtkemaladze <mariamtkemaladze1234@gmail.com>
2024-10-26 17:39:57 +0400

    second commit

---
+++ level 03/classwork/class.py
@@ -0,0 +1,9 @@
+name= 'mary'
+last_name= 'tkemaladze'
+age ='14'
+favourite_place= 'training ground'
+favourite_car= 'Nissan Skyline R34'
+favourite_hobby= 'playing guitar'
+favourite_colour= 'black and navy blue'
+
+print("hello, my name is",  name, "and my surname is", last_name ,"." "my favourite place is", favourite_place, "and my favourite car is", favourite_car, "." "my hobby is", favourite_hobby, "and i love", favourite_colour,"." )
