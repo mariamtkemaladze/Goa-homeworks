33b24bd98879fb3472b99353d72945dedc937a4c
mariamtkemaladze <mariamtkemaladze1234@gmail.com>
2024-11-02 18:17:46 +0400

    second commit

---
+++ level 07/home/homework.py
@@ -0,0 +1,13 @@
+name = "mary"
+last_name = "tkemaladze"
+age = "14"
+country = "Georgia"
+city = "Tbilisi"
+fav_color = "black"
+fav_car = "Nissan Skyline"
+print(f"hello, my name is {name} and my last name is {last_name}. i am {age} years old. i live in {country}, {city}. my favorite color is {fav_color} and my favorite car is {fav_car}.")
+
+
+
+
+
