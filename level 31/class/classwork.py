6a30457b24ea3422ec371eaba8b88c0eee9db699
mariamtkemaladze <mariamtkemaladze1234@gmail.com>
2025-01-28 22:22:45 +0400

    second commit

---
+++ level 31/class/classwork.py
@@ -0,0 +1,5 @@
+info = ["mari", "tkemaladze", "fourteen"]
+
+info_ver = "my name is [0], my surname is [1] and i'm [2] years old."  .format(info[0], info[1], info[2])
+
+print(info_ver)
---
+++ level 31/home/homework.py
@@ -0,0 +1,49 @@
+
+#task 1
+
+
+
+name = "alice"
+place = "wonderland"
+
+formatted_text = f"Hello, {name}. Welcome to {place}."
+
+print(formatted_text)
+
+
+
+#task 2
+
+fruit_string = ", " .join (["apple", "banana", "cherry"])
+
+print(fruit_string)
+
+
+
+#task 3
+
+
+sentence = "The quick brown fox jumps over the lazy dog."
+
+words_list = sentence.split()
+
+print(words_list)
+
+
+#task 4
+
+mixed_case = "PyThOn Is AwEsOmE!"
+
+Lowercase_string = mixed_case.lower()
+
+print(Lowercase_string)
+
+
+
+#task 5
+
+greeting = "good morning"
+
+uppercase_greeting = greeting.upper()
+
+print(uppercase_greeting)
