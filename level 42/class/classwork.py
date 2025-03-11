ad9e969f1c8d6df26d945666714fb7b5ea2eb062
mariamtkemaladze <mariamtkemaladze1234@gmail.com>
2025-03-11 19:16:20 +0400

    second commit

--- level 42/class/classwork.py
+++ level 42/class/classwork.py
@@ -21,3 +21,30 @@
 
 #def past(h, m, s):
    # return 3600000 * h + 60000 * m + 1000 * s
+
+
+#42 part 2 classwork
+
+#1
+
+#Replace all vowel to exclamation mark in the sentence. aeiouAEIOU is vowel.
+
+#def replace_exclamation(st):
+    #  return ''.join('!' if c in 'aeiouAEIOU' else c for c in s)
+
+
+#2
+
+#Given an array of integers.Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.If the input is an empty array or is null, return an empty array.
+
+
+#def count_positives_sum_negatives(arr):
+ #   if not arr: return []
+ #   pos = 0
+   # neg = 0
+  #  for x in arr:
+    #      if x > 0:
+        #          pos += 1
+      #  if x < 0:
+       #     neg += x
+   # return [pos, neg]
